4c566c6e75f22030a22572df8a140bae85f589b7
Richard Han <richard_han@live.com>
2018-12-10 16:06:10 +1300

    First commit
    First complete commit

---
+++ js/scraper.js
@@ -0,0 +1,124 @@
+const http = require('http');
+const cheerio = require('cheerio');
+const request = require('request');
+var tshirts = [];
+var noTshirts;
+
+// Find or create data folder for csv file
+
+/**
+ * Check for 'data' folder
+ * If folder exists
+  * Begin creation of the t-shirt CSV file
+ * If folder does not exist
+  * Create 'data' folder
+  * Begin creation of the t-shirt CSV file
+ */
+var fs = require("fs");
+if (fs.existsSync("./data")) {
+  createTshirtCSV();
+} else {
+  fs.mkdir("./data", function() {
+    createTshirtCSV();
+  });
+}
+
+/** Scrape content from site using module */
+
+/**
+ * Accesses the page which shows all the available t-shirts on the site
+ * If connection to page successful
+  * Accesses the page for each individual t-shirt
+  * Builds array of tshirt JSON objects
+ * If connection to page unsuccessful
+  * Log error message
+ */
+function createTshirtCSV() {
+  request('http://shirts4mike.com/shirts.php', function(err, resp, html) {
+    if (!err) {
+      const $ = cheerio.load(html);
+      $('.products li a').each(function (index, element) {
+        var link = 'http://shirts4mike.com/' + $(element).attr('href');
+        getTshirtInfo(link);
+      });
+    } else {
+      logError("There’s been a 404 error. Cannot connect to http://shirts4mike.com.");
+    }
+  });
+}
+
+/*
+ * Extract information about t-shirt (title, price, image URL) from its page
+ * Convert extracted info + page URL + time of file creation into JSON object
+ * Add JSON object to JSON array of tshirts
+ * If all tshirts have had their information extracted
+ * Create CSV file containing all tshirt information
+ * @param {string} link - The link to a page for a specific t-shirt
+ */
+function getTshirtInfo(link) {
+  request(link, function(err, resp, html) {
+    if (!err) {
+      const $ = cheerio.load(html);
+      var title = $('.breadcrumb').text();
+      title = title.substring(9, title.length);
+      var price = $('.price').text();
+      var imageURL = 'http://shirts4mike.com/' +
+                      $('.shirt-picture span img').attr('src');
+      var time = new Date().toString();
+      var tshirt = {"Title": title,
+                    "Price": price,
+                    "ImageURL": imageURL,
+                    "URL": link,
+                    "Time": time};
+      tshirts.push(tshirt);
+      var date = new Date();
+      var year = date.getFullYear();
+      var month = (date.getMonth() + 1).toString();
+      if (month.length == 1) {
+        month = '0' + month;
+      }
+      var day = date.getDate().toString();
+      if (day.length == 1) {
+        day = '0' + day;
+      }
+      var fileName = year + "-" + month + "-" + day + ".csv";
+      createCSV(fileName, tshirts);
+    }
+  });
+}
+
+/** Create CSV from JSON array */
+
+const json2csvParser = require('json2csv').Parser;
+const fields = ['Title', 'Price', 'ImageURL', 'URL', 'Time'];
+const jsonParse = new json2csvParser({ fields });
+
+/* Save info into CSV named after date of creation
+ * @param {string} fileName - The name of the file that will be written
+ * @param {string} item - JSON object that will be parsed
+ */
+function createCSV(fileName, item) {
+  var filePath = "./data/" + fileName;
+  var csv = jsonParse.parse(item);
+  fs.writeFileSync(filePath, csv, (err) => {
+    if (err) {
+      logError("There's been an error, the file was not saved.");
+    }
+  });
+}
+
+/** Log any errors to file */
+
+/** Builds string consisting of timestamp and error message
+ * Writes the string to a file called scraper-error.log
+ * @param {string} message - The error message that will be written to a file
+ */
+function logError(message) {
+  var date = new Date();
+  console.log(date.toString());
+  var fileText = "[" + date.toString() + "] " + message;
+  var filePath = "scraper-error.log";
+  fs.writeFile(filePath, fileText, (err) => {
+    if (err) throw err;
+  });
+}
